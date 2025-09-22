df46000941e13f7b5a24b35c84c2231e6764f9c2
rishabhsawjann <rishabhsajwan8791@gmail.com>
2025-09-22 16:29:47 +0530

    Objects in JS

---
+++ object2.js
@@ -0,0 +1,16 @@
+// let see how can we remove properties form an object:
+ // there are several ways to remove properties from an object
+ // with the delete operator being the most straightforward
+ // commonly used method.
+ 
+ // when we use delete, it removes the selected property from the 
+ // object. Here's, an exmple of how to use the delete operator:
+
+ const person={
+    name:"Rishabh",
+    age:30,
+    job:"Engineer",
+ };
+ 
+ delete person.job;
+ console.log(person.job);
