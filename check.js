9b8cc2c26559939cf293e896787ad65dc8e28ac1
rishabhsawjann <rishabhsajwan8791@gmail.com>
2025-09-23 10:53:40 +0530

    new

--- check.js
+++ check.js
@@ -4,3 +4,11 @@ let last=arr.pop();
 console.log(first);
 console.log(last);
 console.log(arr);
+// what does pop do?
+// it removes the last element from an array and returns it
+// what does shift do?
+// it removes the first element from an array and returns it
+// what does push do?
+// it adds an element to the end of an array
+// what does unshift do?
+// it adds an element to the beginning of an array
