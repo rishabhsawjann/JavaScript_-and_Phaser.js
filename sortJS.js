10a2bcdf2a8062871d1fbb6c92941a04c5e05e13
rishabhsawjann <rishabhsajwan8791@gmail.com>
2025-09-25 15:26:13 +0530

    Sorting

---
+++ sortJS.js
@@ -0,0 +1,13 @@
+//How do we use sort in JS
+/*
+JS has a built in sort method used to arrange
+the elements of an array and returns
+reference to the sorted array.
+No copy is made because the elements are sorted
+in the same place*/
+
+//array.sort (compareFuntion);//syntax:
+
+const fruits=["Banana","Apple","kela","sab"];
+fruits.sort();
+console.log(fruits);
